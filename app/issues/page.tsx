9a4b7f9da3b47864d88686df60bbb7aad3000e2c
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2023-11-27 19:48:35 +0100

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -6,7 +6,7 @@ import {
   TableHeader,
   TableRowHeaderCell,
 } from "@radix-ui/themes";
-import Link from "next/link";
+import Link from "../components/Link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./IssueActions";
@@ -32,7 +32,9 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <Link href={`/issues/${issue.id}`} >
+                  {issue.title}
+                </Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
