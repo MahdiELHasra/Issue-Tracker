bf9b65a187fa6d7153bb883e8a3e239b43ca01ac
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2023-12-08 18:31:06 +0100

    Lay out the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,21 @@ import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home() {
-  const open = await prisma.issue.count({where:{status:"OPEN"}});
-  const closed = await prisma.issue.count({where:{status:"CLOSED"}})
-  const inProgress = await prisma.issue.count({where:{status:"IN_PROGRESS"}})
-  return <IssueChart open={open} closed={closed} inProgress={inProgress} />;
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+        <IssueChart open={open} closed={closed} inProgress={inProgress} />
+      </Flex>
+      <LatestIssues open={open} closed={closed} inProgress={inProgress} />
+    </Grid>
+  );
 }
