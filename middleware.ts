04b0f7f5773a71dcaad4d6625c1901f86e57323c
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2023-12-12 14:39:34 +0100

    Fix assignedSelect issue

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/issues/new", "/issues/edit/:id+"] };
+export const config = {
+  matcher: ["/issues/new", "/issues/edit/:id+"],
+};
